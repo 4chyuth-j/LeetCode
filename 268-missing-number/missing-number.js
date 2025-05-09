b7d4ea6fc2a5bc4193492cff0b4851d85e6f1839
Achyuth J. <achyuthj2002@gmail.com>
2025-05-09 09:38:31 +0530

    Time: 209 ms (10.45%) | Memory: 55.6 MB (61.93%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let arr = [];
+    for(let i=0; i<=nums.length; i++){
+        arr[i] =i;
+    }
+
+    for(let i=0; i<arr.length; i++){
+        if(!nums.includes(arr[i])){
+            return arr[i];
+        }
+    }
+
+    
+};
