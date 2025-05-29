2a3a66d9a1e8e1148dff75cdbc3297839f55b63e
Achyuth J. <achyuthj2002@gmail.com>
2025-05-29 20:17:58 +0530

    Time: 45 ms (36.08%) | Memory: 53.5 MB (45.95%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -2,7 +2,8 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = val => ({
+var expect = function(val) {
+   return {
     toBe: (comp) => {
         if (val === comp) return true;
         throw new Error("Not Equal");
@@ -11,8 +12,8 @@ var expect = val => ({
         if (val !== comp) return true;
         throw new Error("Equal");
     }
-});
-
+} 
+};
 
 /**
  * expect(5).toBe(5); // true
