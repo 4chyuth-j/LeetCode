0bd3bc175c5b688b7d17e604bd9d95867e778333
Achyuth J. <achyuthj2002@gmail.com>
2025-05-31 00:05:42 +0530

    Time: 35 ms (86.49%) | Memory: 54.6 MB (10.81%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.js
+++ 1970-sorting-the-sentence/sorting-the-sentence.js
@@ -3,19 +3,20 @@
  * @return {string}
  */
 var sortSentence = function(s) {
-     let obj = {};
+    let obj = {};
     let str = s.split(" ");
     for(let item of str ){
         obj[item.slice(-1)]=item.slice(0,item.length-1);
     }
 
-    console.log(obj);
+    //the items in the obj will be in sorted order as the key
 
      str = '';
 
     for(let key in obj){
         str+=obj[key]+" ";
     }
- return str.trim();
+
+    return str.trim();
     
 };
