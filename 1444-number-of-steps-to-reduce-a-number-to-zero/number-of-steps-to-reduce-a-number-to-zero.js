e1a6c6d474df7a01e82526ca5ca766ae13f435b3
Achyuth J. <achyuthj2002@gmail.com>
2025-06-09 09:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (95.65%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -2,16 +2,28 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps = function(num) {
-    function counter(n,steps){
-        if(n==0) return steps;
+var numberOfSteps = function (num) {
+    // function counter(n,steps){
+    //     if(n==0) return steps;
 
-        if(n%2==0) return counter(n/2, ++steps);
-        else return counter(n-1, ++steps);
+    //     if(n%2==0) return counter(n/2, ++steps);
+    //     else return counter(n-1, ++steps);
+    // }
+
+    // return counter(num,0);
+
+
+    let steps = 0;
+    while (num != 0) {
+        steps++;
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = num - 1;
+        }
     }
 
-    return counter(num,0);
+    return steps
 
-
-    
 };
