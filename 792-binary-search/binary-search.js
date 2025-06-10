7c032020d4e593a7ee2c2b18663d7ba0452a7817
Achyuth J. <achyuthj2002@gmail.com>
2025-06-10 13:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (29.53%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,38 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    // let start = 0;
-    // let end = nums.length-1;
-    // while(start<=end){
-    //     mid = Math.floor((start+end)/2);
-    //     if(nums[mid]==target){
-    //         return mid;
-    //     }
-    //     if(nums[mid]>target){
-    //         end = mid-1;
-    //     } else {
-    //         start = mid+1;
-    //     }
-    // }
-
-    // return -1;
-
-    function findelem(nums,target,start,end){
-        if(start>end) return -1;
-        let mid = Math.floor((start+end)/2);
+    let start = 0;
+    let end = nums.length-1;
+    while(start<=end){
+        mid = Math.floor((start+end)/2);
         if(nums[mid]==target){
             return mid;
-        } else if(nums[mid]>target){
+        }
+        if(nums[mid]>target){
             end = mid-1;
-            return findelem(nums,target,start,end);
         } else {
             start = mid+1;
-            return findelem(nums,target,start,end);
         }
-   
     }
 
-    return findelem(nums,target,0,nums.length-1);
-
+    return -1;
 
 };
