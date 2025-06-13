279127e42911a752ae3c4e7140b1ad1f4adbd7c9
Achyuth J. <achyuthj2002@gmail.com>
2025-06-13 10:59:47 +0530

    Time: 10 ms (35.48%) | Memory: 64.8 MB (43.48%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,24 +3,37 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let flag=0;
-   let testvar1=String(x);
-   let size=testvar1.length;
+//     let flag=0;
+//    let testvar1=String(x);
+//    let size=testvar1.length;
 
 
-    for(let i=0; i<size/2; i++){
-        if(testvar1[i]!=testvar1[size-i-1]){
+//     for(let i=0; i<size/2; i++){
+//         if(testvar1[i]!=testvar1[size-i-1]){
           
-          flag++;
-          break;
+//           flag++;
+//           break;
            
-        }
-    }
+//         }
+//     }
 
-    if(flag!=0){
-        return  false;
-    }else{
-        return true;
+//     if(flag!=0){
+//         return  false;
+//     }else{
+//         return true;
+//     }
+
+let item = String(x);
+let start = 0;
+let end = item.length-1;
+while(start<end){
+    if(item[start]===item[end]){
+        start++;
+        end--;
+    } else {
+        return false
     }
+}
+return true;
 
 };
