2eb4dd16a4a63bcd9f8019e1a1f8cc0a5737d9b9
Achyuth J. <achyuthj2002@gmail.com>
2025-06-14 09:10:40 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (63.30%) - LeetSync

--- 50-powx-n/powx-n.js
+++ 50-powx-n/powx-n.js
@@ -3,23 +3,22 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function (x, n) {
-    // if (n == 0) return 1;
-    // if (x == 0) return 0;
+var myPow = function(x, n) {
+    if(x==0) return 0;
+    if(n==0) return 1;
 
-    function pow(num, n) {
-        if (n === 0) return 1;
-        let half = pow(num, Math.floor(n / 2));
-        if (n % 2 === 0) {
-            return half * half;
+    function pow(num,n){
+        if(n==0) return 1;
+        let half = pow(num,Math.floor(n/2));
+        if(n%2==0){
+            return half*half;
         } else {
-            return num * half * half;
+            return num * half*half;
         }
     }
-
-    if (n > 0) {
-        return pow(x, n);
+    if(n>0){
+        return pow(x,n);
     } else {
-        return pow(1 / x, -n);
+        return pow(1/x,-n);
     }
 };
