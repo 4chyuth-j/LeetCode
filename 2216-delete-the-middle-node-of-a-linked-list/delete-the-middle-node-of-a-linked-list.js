f2f6cf0bd9859c3fa8e7d78ac1d23f12742690cb
Achyuth J. <achyuthj2002@gmail.com>
2025-06-20 13:08:44 +0530

    Time: 3 ms (92.00%) | Memory: 104 MB (5.58%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.js
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.js
@@ -15,16 +15,26 @@ var deleteMiddle = function(head) {
         return head;
     }
 
+    // let slow = head;
+    // let fast = head;
+    // let prev = null;
+    // while(fast!=null && fast.next!=null){
+    //     prev = slow;
+    //     slow = slow.next;
+    //     fast = fast.next.next;
+    // }
+    // prev.next = prev.next.next;
+    // slow.next = null; // remove
+    // return head;
+
     let slow = head;
-    let fast = head;
-    let prev = null;
+    let fast = head.next.next;
     while(fast!=null && fast.next!=null){
-        prev = slow;
-        slow = slow.next;
-        fast = fast.next.next;
+        slow= slow.next;
+        fast= fast.next.next;
     }
-    prev.next = prev.next.next;
-    slow.next = null; // remove
+
+    slow.next = slow.next.next;
     return head;
      
 
