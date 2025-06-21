d7694522f31e985f4858ddb469772fc02b6730d2
Achyuth J. <achyuthj2002@gmail.com>
2025-06-21 10:37:11 +0530

    Time: 12 ms (31.66%) | Memory: 56.1 MB (79.94%) - LeetSync

---
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxK = function(nums) {
+    nums.sort((a,b)=>b-a);
+    // for(let i=0; i<nums.length; i++){
+    //     if(nums.includes(-nums[i]))
+    // }
+    let i=0;
+    while(nums[i]>0 && i<nums.length){
+        if(nums.includes(-nums[i])){
+            return nums[i];
+        }
+        i++;
+    }
+    return -1;
+};
