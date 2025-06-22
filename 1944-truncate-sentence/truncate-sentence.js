b9a2975169761ad9862be11291f3ce3a0431949b
Achyuth J. <achyuthj2002@gmail.com>
2025-06-22 11:33:05 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (19.19%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.js
+++ 1944-truncate-sentence/truncate-sentence.js
@@ -4,8 +4,12 @@
  * @return {string}
  */
 var truncateSentence = function(s, k) {
-    let str = s.split(" ");
-    newStr=str.slice(0,k);
-    newStr = newStr.join(" ");
-    return newStr;
+    // let str = s.split(" ");
+    // newStr=str.slice(0,k);
+    // newStr = newStr.join(" ");
+    // return newStr;
+
+    let str = s.split(" ").slice(0,k).join(" ");
+    return str;
+
 };
