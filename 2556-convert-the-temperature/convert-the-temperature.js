a4e00ea455be0f58a7764a632162749eb2a6966d
Achyuth J. <achyuthj2002@gmail.com>
2025-06-23 15:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (57.37%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.js
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -3,8 +3,7 @@
  * @return {number[]}
  */
 var convertTemperature = function(Celsius) {
-    
-    let Kelvin = Celsius + 273.15;
+     let Kelvin = Celsius + 273.15;
     
     let Fahrenheit = Celsius * 1.80 + 32.00;
     
