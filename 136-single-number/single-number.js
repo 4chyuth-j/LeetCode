277a309521c62e0995c685760b64899bc748eb5d
Achyuth J. <achyuthj2002@gmail.com>
2025-06-26 16:30:37 +0530

    Time: 16 ms (16.94%) | Memory: 59.9 MB (15.45%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,20 +3,33 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let flag = 0;
-    for(let i=0; i<nums.length; i++){
-        flag=0;
-        for(let j=0; j<nums.length; j++){
-            if(i==j){
-                continue;
-            }
-            if(nums[i]==nums[j]){
-                flag++;
-            }
-        }
+    // let flag = 0;
+    // for(let i=0; i<nums.length; i++){
+    //     flag=0;
+    //     for(let j=0; j<nums.length; j++){
+    //         if(i==j){
+    //             continue;
+    //         }
+    //         if(nums[i]==nums[j]){
+    //             flag++;
+    //         }
+    //     }
 
-        if(flag==0){
-            return nums[i];
+    //     if(flag==0){
+    //         return nums[i];
+    //     }
+    // }
+
+    let result = {};
+    for(let i=0; i<nums.length; i++){
+        if(result[nums[i]]){
+            result[nums[i]]++;
+        } else {
+            result[nums[i]]=1;
         }
+    }
+
+    for(let num in result){
+        if(result[num] ==1) return Number(num);
     }
 };
