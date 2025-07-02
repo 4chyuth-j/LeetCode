5f1ce6e011d8f850c14c7f1f6071e9d3acc5d1c3
Achyuth J. <achyuthj2002@gmail.com>
2025-07-02 12:48:36 +0530

    Time: 5 ms (24.07%) | Memory: 56.1 MB (41.40%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -4,31 +4,28 @@
  */
 var isValid = function (s) {
     
-    
-    let arr = [[')', '('], [']', '['], ['}', '{']];
+     let arr = ['(', '[', '{'];
+    let opening = new Set(arr);
+    arr = [[')', '('], [']', '['], ['}', '{']];
     let closing = new Map(arr);
 
     let stack = [];
 
     for (let i = 0; i < s.length; i++) {
-        if (!closing.has(s[i])) {
-
+        if (opening.has(s[i])) {
             stack.push(s[i]);
-
         } else {
-
-            if(stack.length==0) return false;
+            if(stack.length == 0) return false;
 
             if (stack.pop() == closing.get(s[i])) {
                 continue;
             } else {
                 return false;
             }
-
         }
     }
 
-    if(stack.length!=0) return false;
+    if(stack.length != 0) return false;
 
     return true;
 
