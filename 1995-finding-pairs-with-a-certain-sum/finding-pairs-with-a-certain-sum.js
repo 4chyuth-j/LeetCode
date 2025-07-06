83a1671a2e16b75607475c66e5ecaab9b5277df4
Achyuth J. <achyuthj2002@gmail.com>
2025-07-06 09:55:29 +0530

    Time: 102 ms (84.21%) | Memory: 86.9 MB (42.10%) - LeetSync

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ */
+var FindSumPairs = function (num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.freqMap = new Map();
+
+    //initialising frequency map for num2
+    for (const num of num2) {
+        this.freqMap.set(num, (this.freqMap.get(num) || 0) + 1);
+    }
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+FindSumPairs.prototype.add = function (index, val) {
+    let oldval = this.num2[index];
+    if (this.freqMap.has(oldval)) {
+        this.freqMap.set(oldval, this.freqMap.get(oldval) - 1);
+    }
+
+    this.num2[index] += val;
+    let newval = this.num2[index];
+
+    this.freqMap.set(newval, (this.freqMap.get(newval) || 0) + 1);
+};
+
+/** 
+ * @param {number} tot
+ * @return {number}
+ */
+FindSumPairs.prototype.count = function (tot) {
+    let pairCount = 0;
+    for (let num of this.num1) {
+        let target = tot - num;
+        pairCount += this.freqMap.get(target) || 0;
+    }
+    return pairCount;
+};
+
+/** 
+ * Your FindSumPairs object will be instantiated and called as such:
+ * var obj = new FindSumPairs(nums1, nums2)
+ * obj.add(index,val)
+ * var param_2 = obj.count(tot)
+ */
