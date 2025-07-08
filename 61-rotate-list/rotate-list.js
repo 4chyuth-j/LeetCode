5bee513f49555a3408f9b7a3f1b3cccacdd540bc
Achyuth J. <achyuthj2002@gmail.com>
2025-07-08 09:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (10.98%) - LeetSync

--- 61-rotate-list/rotate-list.js
+++ 61-rotate-list/rotate-list.js
@@ -11,6 +11,7 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
+    
     if(head==null || head.next==null) return head;
     let length = 0;
     let curr = head;
@@ -22,8 +23,8 @@ var rotateRight = function(head, k) {
     k = k%length;
 
     for(let i=1; i<=k; i++){
-        let headOld = head;
-        let last = headOld;
+        
+        let last = head;
         while(last.next.next!=null){
             last = last.next;
         }
@@ -32,31 +33,9 @@ var rotateRight = function(head, k) {
         last.next = null;
         rotateElem.next = head;
         head = rotateElem;
+
+        
     }
     return head;
-    // if(head==null || head.next==null) return head;
-    // let length = 0;
-    // let curr = head;
-    // while(curr){
-    //     length++;
-    //     curr = curr.next;
-    // }
 
-    // k = k%length;
-
-    // for(let i=1; i<=k; i++){
-        
-    //     let last = head;
-    //     while(last.next.next!=null){
-    //         last = last.next;
-    //     }
-
-    //     let rotateElem = last.next;
-    //     last.next = null;
-    //     rotateElem.next = head;
-    //     head = rotateElem;
-
-        
-    // }
-    // return head;
 };
