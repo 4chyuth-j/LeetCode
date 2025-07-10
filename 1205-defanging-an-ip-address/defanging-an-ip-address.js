c58355af97cba7d58e32f062e96fdcb682fbaabc
Achyuth J. <achyuthj2002@gmail.com>
2025-07-10 15:50:32 +0530

    Time: 37 ms (75.10%) | Memory: 52.7 MB (85.69%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.js
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.js
@@ -3,5 +3,14 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    return address.split('.').join('[.]');
+    let st = [];
+    for(let i=address.length-1; i>=0; i--){
+        if(address[i]=='.') st.push('[.]');
+        else st.push(address[i]);
+    }
+    let result = '';
+    while(st.length!=0){
+        result+=st.pop();
+    }
+    return result;
 };
