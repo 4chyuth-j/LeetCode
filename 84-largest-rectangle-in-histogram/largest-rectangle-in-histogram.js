110a3e133b7889bca5845aba72c93826d930f293
Achyuth J. <achyuthj2002@gmail.com>
2025-07-11 11:23:22 +0530

    Time: 76 ms (10.00%) | Memory: 80.6 MB (24.41%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -0,0 +1,90 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    // let maxArea = 0;
+    // for(let i=0; i<heights.length; i++){
+    //     let minHeight = heights[i];
+    //     let width =0;
+    //     for(let j=i; j<heights.length; j++){
+    //         width +=1;
+    //         if(heights[j]<minHeight){
+    //             minHeight = heights[j];
+    //         }
+    //         let area = minHeight*width;
+    //         if(maxArea < area) {
+    //             maxArea = area;
+    //         }
+    //     }
+    // }
+
+    // return maxArea;
+
+    let left = [];
+    let right = [];
+    let stack = [];
+    
+
+    for(let i=0; i<heights.length; i++){
+        while(stack.length>0 && heights[stack[stack.length-1]]>=heights[i]){
+            stack.pop();
+        }
+
+        if(stack.length==0){
+            left[i] = -1;
+        } else {
+            left[i] = stack[stack.length-1];
+        }
+
+        stack.push(i);
+
+    }
+
+    while(stack.length!=0){
+        stack.pop();
+    }
+
+
+    for(let i=heights.length-1; i>=0; i--){
+        while(stack.length>0 && heights[stack[stack.length-1]]>= heights[i]){
+            stack.pop();
+        }
+
+        if(stack.length==0){
+            right[i] = heights.length;
+        } else {
+            right[i] = stack[stack.length-1];
+        }
+
+        stack.push(i);
+    }
+    
+
+    let maxArea = 0;
+    
+    for(let i=0; i<heights.length; i++){
+        let width =  right[i] - left[i] -1
+        let area = heights[i] * width;
+        maxArea  =  Math.max(maxArea,area);
+    }
+
+    return maxArea;
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
