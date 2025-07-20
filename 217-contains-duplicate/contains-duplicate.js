d625aad9a7ac28698e86dcf00dcecac39395ed99
Achyuth J. <achyuthj2002@gmail.com>
2025-07-20 10:40:28 +0530

    Time: 20 ms (49.19%) | Memory: 73.5 MB (69.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -4,9 +4,5 @@
  */
 var containsDuplicate = function(nums) {
    let set = new Set(nums);
-   if(set.size == nums.length){
-    return false;
-   } else {
-    return true;
-   }
+   return set.size!== nums.length;
 };
