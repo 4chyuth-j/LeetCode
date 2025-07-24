27930cee249a689d89e9dbb5ce13e3438521491c
Achyuth J. <achyuthj2002@gmail.com>
2025-07-24 14:06:08 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (28.85%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -3,9 +3,5 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-  let res = '';
-    for(let i=0; i<s.length; i++){
-        res+=s[i].toLowerCase();
-    }
-    return res;
+ return s.toLowerCase();
 };
