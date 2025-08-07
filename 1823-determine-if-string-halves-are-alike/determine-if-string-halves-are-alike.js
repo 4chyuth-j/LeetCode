286382ce8a33f70498bb91197137ccfb99e6e14d
Achyuth J. <achyuthj2002@gmail.com>
2025-08-07 10:02:25 +0530

    Time: 2 ms (84.25%) | Memory: 53.5 MB (94.52%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var halvesAreAlike = function (s) {
+    let set = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    let count =0;
+    let start =0,end = s.length-1;
+    while(start<end){
+        if(set.has(s[start])) count++;
+        if(set.has(s[end])) count--;
+        start++;
+        end--;
+    }
+
+    if(count==0) return true;
+    else return false;
+};
