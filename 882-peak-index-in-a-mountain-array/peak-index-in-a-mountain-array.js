5698d21e390153b2184528d07a383f73f8ee0908
Achyuth J. <achyuthj2002@gmail.com>
2025-08-15 11:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 62.3 MB (99.36%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    
+    let start = 1;
+    let end = arr.length-2;
+    while(start<=end){
+        let mid = Math.floor((start+end)/2);
+        if(arr[mid-1]< arr[mid] && arr[mid]> arr[mid+1]) return mid;
+        else if(arr[mid-1]<arr[mid]) start = mid+1;
+        else end = mid-1;
+    }
+};
