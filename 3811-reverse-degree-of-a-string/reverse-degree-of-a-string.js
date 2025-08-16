0c00e94b1d78dfa9d786fd70050026d6e02ca155
Achyuth J. <achyuthj2002@gmail.com>
2025-08-16 09:45:49 +0530

    Time: 1 ms (82.55%) | Memory: 55.8 MB (65.53%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
@@ -4,9 +4,22 @@
  */
 var reverseDegree = function(s) {
     let res = 0;
-    for(let i=0; i<s.length; i++){
-        res +=(123-s[i].charCodeAt(0))*(i+1);
-    }    
+    // for(let i=0; i<s.length; i++){
+    //     res +=(123-s[i].charCodeAt(0))*(i+1);
+    // }  
+
+    let left = 0, right = s.length-1;
+
+    while(left<=right){
+        if(left==right){
+            res+=(123-s[left].charCodeAt(0))*(left+1);
+        } else {
+            res+=(123-s[right].charCodeAt(0))*(right+1);
+            res+=(123-s[left].charCodeAt(0))*(left+1);
+        }
+        left++;
+        right--;
+    }  
 
     return res;
 };
