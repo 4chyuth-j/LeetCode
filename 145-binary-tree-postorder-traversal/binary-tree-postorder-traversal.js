bd11f0fc574025cc5c72f1ae2e165018daf84539
Achyuth J. <achyuthj2002@gmail.com>
2025-08-23 09:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 51.1 MB (99.94%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let res = [];
+    postOrder(root,res);
+    return res;
+};
+
+function postOrder(root,res){
+    if(root!=null){
+        postOrder(root.left,res);
+        postOrder(root.right,res);
+        res.push(root.val);
+    }
+}
