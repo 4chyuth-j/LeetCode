15b1726285fd35e9007bb96e9b262dafb040e865
Achyuth J. <achyuthj2002@gmail.com>
2025-09-09 09:56:21 +0530

    Time: 1 ms (80.43%) | Memory: 54.4 MB (68.50%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -2,23 +2,19 @@
  * @param {string[]} operations
  * @return {number}
  */
-var calPoints = function(op) {
+var calPoints = function (op) {
     let res = [];
-    const regex = /^[CD+]$/;
-   for(let i=0; i<op.length; i++){
-    if(regex.test(op[i])){
-        if(op[i]=='C'){
+    for (let i = 0; i < op.length; i++) {
+        if (op[i] == 'C') {
             res.pop();
-        } else if(op[i]=='D'){
-            res.push(res[res.length-1]*2);
-        } else {
-            let sum = res[res.length-1]+res[res.length-2];
+        } else if (op[i] == 'D') {
+            res.push(res[res.length - 1] * 2);
+        } else if (op[i] == '+') {
+            let sum = res[res.length - 1] + res[res.length - 2];
             res.push(sum);
+        } else {
+            res.push(parseInt(op[i]));
         }
-
-    } else {
-        res.push(parseInt(op[i]));
     }
-   }
-   return res.reduce((acc,curr)=>acc+curr,0);
+    return res.reduce((acc, curr) => acc + curr, 0);
 };
