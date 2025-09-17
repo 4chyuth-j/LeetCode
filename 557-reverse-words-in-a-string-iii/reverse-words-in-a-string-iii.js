1560da97c0d2ccc058270e904e75bf98dbb53a19
Achyuth J. <achyuthj2002@gmail.com>
2025-09-17 09:55:08 +0530

    Time: 11 ms (53.73%) | Memory: 60.5 MB (62.58%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -4,20 +4,24 @@
  */
 var reverseWords = function(s) {
     let res = s.split(" ");
+
+    for(let i=0; i<res.length; i++){
+        res[i] = res[i].split('').reverse().join('');
+    }
     
 
-    for(let i=0; i<res.length;i++){
-        let out = res[i].split('');
-        console.log(out);
+    // for(let i=0; i<res.length;i++){
+    //     let out = res[i].split('');
         
-        let len = out.length;
-        for(let j=0; j<out.length/2; j++){
-            [out[j],out[len-1-j]]=[out[len-1-j],out[j]];
-        }
-        console.log(out);
+        
+    //     let len = out.length;
+    //     for(let j=0; j<out.length/2; j++){
+    //         [out[j],out[len-1-j]]=[out[len-1-j],out[j]];
+    //     }
+        
 
-        res[i] = out.join("");
-    }
+    //     res[i] = out.join("");
+    // }
     
 
     return res.join(" ");
