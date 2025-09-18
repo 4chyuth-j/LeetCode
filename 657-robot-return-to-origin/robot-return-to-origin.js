b5dfacf7963f738c5cab5ba5cf7e42575db2ee70
Achyuth J. <achyuthj2002@gmail.com>
2025-09-18 11:45:27 +0530

    Time: 3 ms (93.38%) | Memory: 55 MB (81.80%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.js
+++ 657-robot-return-to-origin/robot-return-to-origin.js
@@ -6,23 +6,27 @@ var judgeCircle = function(moves) {
 
     let res = [0,0];
     for(let i=0; i<moves.length; i++){
-        if(moves[i]=="U" || moves[i]=='D'){
+        // if(moves[i]=="U" || moves[i]=='D'){
 
-            if(moves[i]=='U'){
-                res[1]+=1;
-            } else {
-                res[1]-=1;
-            }
+        //     if(moves[i]=='U'){
+        //         res[1]+=1;
+        //     } else {
+        //         res[1]-=1;
+        //     }
 
-        } else {
+        // } else {
 
-            if(moves[i]=='R'){
-                res[0]+=1;
-            } else{
-                res[0]-=1;
-            }
+        //     if(moves[i]=='R'){
+        //         res[0]+=1;
+        //     } else{
+        //         res[0]-=1;
+        //     }
 
-        }
+        // }
+         if (moves[i] === 'L') res[0]-=1;
+        else if (moves[i] === 'R')  res[0]+=1;
+        else if (moves[i] === 'U') res[1]+=1;
+        else if (moves[i] === 'D') res[1]-=1;
     }
 
     return res[0]==0 && res[1]==0
