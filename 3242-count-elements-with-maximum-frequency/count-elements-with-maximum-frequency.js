74433f44b6a39b6e6ca7673260cd583ffdee5476
Achyuth J. <achyuthj2002@gmail.com>
2025-09-22 09:57:11 +0530

    Time: 26 ms (6.48%) | Memory: 60.4 MB (5.67%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxFrequencyElements = function(nums) {
+    if(nums.length==1) return 1;
+
+    let freq = new Map();
+    for(let i=0; i<nums.length; i++){
+        freq.set(nums[i],(freq.get(nums[i])||0) + 1 );
+    }
+    console.log(freq);
+    let freqArr = [...freq.values()];
+    freqArr.sort((a,b)=>b-a);
+    console.log(freqArr);
+    let ref = freqArr[0];
+    let res =freqArr[0];
+    for(let i=1; i<freqArr.length; i++){
+        if(freqArr[i]==ref){
+            res+=freqArr[i];
+        }
+    }
+
+    return res;
+
+};
