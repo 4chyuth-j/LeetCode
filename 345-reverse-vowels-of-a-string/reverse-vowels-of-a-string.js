558055386c6f08e34efc623f35bf13a4f3a8d230
Achyuth J. <achyuthj2002@gmail.com>
2025-09-23 09:19:42 +0530

    Time: 10 ms (34.96%) | Memory: 60.9 MB (49.35%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -3,26 +3,41 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    let res = s.split("");
-    let vowel = [];
+    // let res = s.split("");
+    // let vowel = [];
 
-    for(let i=0; i<s.length; i++){
-        if("aeiouAEIOU".includes(s[i])){
-            vowel.push(s[i]);
-        }
+    // for(let i=0; i<s.length; i++){
+    //     if("aeiouAEIOU".includes(s[i])){
+    //         vowel.push(s[i]);
+    //     }
+    // }
+
+    // if(vowel.length==0) return s;
+
+    // let ind=vowel.length-1;
+
+    // for(let i=0; i<res.length; i++){
+    //     if('aeiouAEIOU'.includes(res[i])){
+    //         res[i] = vowel[ind];
+    //         ind--;
+    //     }
+    // }
+
+    // return res.join('');
+
+
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+    let arr = s.split('');
+    let start = 0, end = arr.length-1;
+
+    while(start<end){
+        while(start<end && !vowels.has(arr[start])) start++;
+        while(start<end && !vowels.has(arr[end])) end--;
+        [arr[start],arr[end]] = [arr[end],arr[start]];
+        start++;
+        end--;
     }
 
-    if(vowel.length==0) return s;
-
-    let ind=vowel.length-1;
-
-    for(let i=0; i<res.length; i++){
-        if('aeiouAEIOU'.includes(res[i])){
-            res[i] = vowel[ind];
-            ind--;
-        }
-    }
-
-    return res.join('');
+    return arr.join('');
     
 };
