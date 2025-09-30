a487866092baa6564d484480b033775f8e11b967
Achyuth J. <achyuthj2002@gmail.com>
2025-09-30 12:10:37 +0530

    Time: 53 ms (39.44%) | Memory: 57.9 MB (5.28%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.js
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.js
@@ -10,7 +10,7 @@ var minOperations = function(nums) {
     for(let i=0; i<nums.length-1; i++){
         if(nums[i]>=nums[i+1]){
             res +=nums[i]-nums[i+1]+1;
-            nums[i+1] += nums[i]-nums[i+1]+1;
+            nums[i+1] = nums[i]+1;
         }
     }
     return res;
