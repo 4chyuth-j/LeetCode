16e7df031385c8dca30677db5c0723c6bd98d991
Achyuth J. <achyuthj2002@gmail.com>
2025-10-10 09:26:30 +0530

    Time: 8 ms (89.09%) | Memory: 54.7 MB (97.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,19 +5,35 @@
  */
 var isAnagram = function(s, t) {
     
-     if (s.length !== t.length) return false;
+    //  if (s.length !== t.length) return false;
 
-    const count = {};
+    // const count = {};
 
-    for (let char of s) {
-        count[char] = (count[char] || 0) + 1;
+    // for (let char of s) {
+    //     count[char] = (count[char] || 0) + 1;
+    // }
+
+    // for (let char of t) {
+    //     if (!count[char]) return false;
+    //     count[char]--;
+    // }
+
+    // return true;
+
+    if(s.length!= t.length) return false;
+
+    let counter = new Array(26).fill(0);
+
+    for(let i=0; i<s.length; i++){
+        counter[s.charCodeAt(i)-'a'.charCodeAt(0)]++;
+        counter[t.charCodeAt(i)-'a'.charCodeAt(0)]--;
     }
 
-    for (let char of t) {
-        if (!count[char]) return false;
-        count[char]--;
+    for(let char of counter){
+        if(char!=0){
+            return false;
+        }
     }
 
     return true;
-    
 };
