28bd7907b56724cf3ed10456dff424efc0a4e2db
Achyuth J. <achyuthj2002@gmail.com>
2025-10-12 13:34:51 +0530

    Time: 3 ms (61.59%) | Memory: 57.8 MB (17.20%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,31 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map = new Map();
 
-    // let arr=[];
-    // outerloop: for(let i=0; i<nums.length-1; i++){
-
-    //     for(let j=i+1; j<nums.length; j++){
-
-    //         if(nums[i]+nums[j]==target){
-               
-    //            arr=[i,j];
-
-    //            break outerloop;
-
-    //         }
-
-    //     }
-    // }
-    //    return arr;
-
-    let testMap = new Map();
     for(let i=0; i<nums.length; i++){
-        if(testMap.has(target-nums[i])){
-            return [testMap.get(target-nums[i]),i];
+        if(map.has(nums[i])){
+            return [map.get(nums[i]), i];
         } else {
-            testMap.set(nums[i],i);
+            map.set(target-nums[i], i);
         }
-    }
-    
+    } 
 };
