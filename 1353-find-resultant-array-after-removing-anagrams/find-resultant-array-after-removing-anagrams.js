5393dbf17b1bd48359061ed4d94382561ef0f57e
Achyuth J. <achyuthj2002@gmail.com>
2025-10-13 09:54:10 +0530

    Time: 4 ms (98.17%) | Memory: 62.1 MB (17.43%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.js
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.js
@@ -3,45 +3,20 @@
  * @return {string[]}
  */
 var removeAnagrams = function(words) {
-    if(words.length==0) return words
+    
+    if(words.length==0) return words;
+    let result = [];
+    let prevItem = ''
 
-    for(let i=0; i<words.length-1; i++){
-        if(isAnagram(words[i],words[i+1])){
-            console.log(`${words[i]} and ${words[i+1]} are anagram`);
-            words.splice(i+1,1);
-            i--;
+    for(let word of words){
+        let item = word.split('').sort().join('');
+        if(item!=prevItem){
+            result.push(word);
+            prevItem = item;
         }
     }
 
-    return words;
-    
+    return result;
+
 };
 
-function isAnagram(word1,word2){
-    
-    let map = new Map();
-    for(let i=0; i<word1.length; i++){
-        map.set(word1[i],(map.get(word1[i]) || 0) + 1 );
-    }
-    
-
-    for(let i=0; i<word2.length; i++){
-        if(map.has(word2[i])){
-
-            if(map.get(word2[i])==0){
-                return false;
-            }
-
-            map.set(word2[i],(map.get(word2[i]) - 1 ));
-        } else {
-            return false;
-        }
-    }
-    
-
-    for(let val of map.values()){
-        if(val!=0) return false;
-    }
-
-    return true;
-}
