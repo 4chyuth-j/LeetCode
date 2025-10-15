a93767a05cd4cb2f8fc7a2c1f7bddb448c6833d3
Achyuth J. <achyuthj2002@gmail.com>
2025-10-15 10:03:01 +0530

    Time: 18 ms (30.20%) | Memory: 60 MB (48.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,113 +4,73 @@
  * @return {number[]}
  */
 
-
-
-class priQueue{
+ class MyPriorityQueue{
     constructor(){
-        this.heap = [];
+        this.queue = [];
+        
+    }
+
+    getLength(){
+        return this.queue.length;
     }
 
     isEmpty(){
-        return this.heap.length == 0;
+        return this.queue.length == 0;
     }
 
-    getLen(){
-        return this.heap.length;
-    }
+    enqueue(element){
+        if(this.isEmpty()){
+            this.queue.push(element)
+        } else {
+            let added = false;
+            for(let i=0; i<this.getLength(); i++){
+                if(element[1]>this.queue[i][1]){
+                    this.queue.splice(i,0,element);
+                    added = true;
+                    break;
+                }
+            }
 
-    getParent(ind){
-        return Math.floor((ind-1)/2);
-    }
-
-    leftChild(ind){
-        return (ind*2)+1;
-    }
-
-    rightChild(ind){
-        return (ind*2)+2;
-    }
-
-    swap(ind1,ind2){
-        [this.heap[ind1],this.heap[ind2]] = [this.heap[ind2],this.heap[ind1]];
-    }
-
-    insert(key,value){
-        let elem = {key,value};
-        this.heap.push(elem);
-        this.upheap(this.getLen()-1);
-    }
-
-    upheap(ind){
-        if(ind == 0) return ;
-        let parInd = this.getParent(ind);
-        if(this.heap[parInd].value < this.heap[ind].value){
-            this.swap(parInd,ind);
-            this.upheap(parInd);
+            if(added==false){
+                this.queue.push(element);
+            }
         }
     }
 
-    remove(){
-        if(this.isEmpty()) return null;
-        let elem = this.heap[0];
-        let last = this.heap.pop();
-
-        if(this.getLen()>0){
-            this.heap[0] = last;
-            this.downheap(0);
-        }
-
-        return elem.key;
-
-    }
-
-    downheap(ind){
-        let max = ind;
-        let left = this.leftChild(ind);
-        let right = this.rightChild(ind);
-
-        if(left<this.getLen() && this.heap[left].value>this.heap[max].value){
-            max = left;
-        }
-
-        if(right<this.getLen() && this.heap[right].value> this.heap[max].value){
-            max = right;
-        }
-
-        if(max!= ind){
-            this.swap(max,ind);
-            this.downheap(max);
-        }
+    dequeue(){
+        let elem = this.queue.shift();
+        return elem[0];
     }
 
 
-
-}
+ }
 
 
 
 
 var topKFrequent = function(nums, k) {
     let map = new Map();
+    let res = [];
     for(let item of nums){
         map.set(item,(map.get(item) || 0)+1 );
     }
 
-    return topFreq(map,k);
+    let pQueue = new MyPriorityQueue();
 
-};
-
-function topFreq(map,k){
-    let result = [];
-    let pq = new priQueue();
-    for(let [key,value] of map){
-        pq.insert(key,value);
+    for(let [key,val] of map){
+        pQueue.enqueue([key,val]);
     }
-    while(k>0 && !pq.isEmpty()){
-        result.push(pq.remove());
+
+    while(k>0 && !pQueue.isEmpty()){
+        res.push(pQueue.dequeue());
         k--;
     }
 
-    return result;
-}
+    return res;
+
+    
+
+};
+
+
 
