6773ab1a54e0e30d2f254f67e3ea030c6dd10cd8
Achyuth J. <achyuthj2002@gmail.com>
2025-10-16 10:08:07 +0530

    Time: 6 ms (63.93%) | Memory: 70.2 MB (65.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -4,26 +4,26 @@
  */
 var productExceptSelf = function(nums) {
     let res = [];
-    let pref = [];
-    let suf = [];
+    
+    // let suf = [];
 
     let start1 =1;
     for(let i=0; i<nums.length; i++){
-        pref.push(start1);
+        res.push(start1);
         start1 = start1*nums[i];
     }
 
     let start2 = 1;
     for(let i=nums.length-1; i>=0; i--){
-        suf.unshift(start2);
+        res[i] = res[i]*start2;
         start2 = start2*nums[i];
     }
-    console.log(pref)
-    console.log(suf)
+    // console.log(pref)
+    // console.log(suf)
 
-    for(let i=0; i<nums.length; i++){
-        res.push(pref[i]*suf[i]);
-    }
+    // for(let i=0; i<nums.length; i++){
+    //     res.push(pref[i]*suf[i]);
+    // }
 
     return res;
 };
