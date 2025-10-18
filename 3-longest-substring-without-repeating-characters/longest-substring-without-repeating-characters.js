ed151714c88faaa121783dd5ce52c45a68031450
Achyuth J. <achyuthj2002@gmail.com>
2025-10-18 10:01:15 +0530

    Time: 119 ms (13.33%) | Memory: 61.7 MB (9.86%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let res = 0;
+    for (let j = 0; j < s.length; j++) {
+        let  charSet = new Set();
+        for (let i = j; i < s.length; i++) {
+           if(charSet.has(s[i])){
+                break;
+           } else {
+             charSet.add(s[i]);
+             res = Math.max(res,charSet.size);
+           }
+        }
+        
+    }
+    return res;
+};
