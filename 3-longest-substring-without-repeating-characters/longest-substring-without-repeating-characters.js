7852e083adbf7ba005c7ef7c8c5b53a6f5360932
Achyuth J. <achyuthj2002@gmail.com>
2025-10-21 10:22:39 +0530

    Time: 5 ms (78.99%) | Memory: 57.2 MB (69.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -4,20 +4,19 @@
  */
 var lengthOfLongestSubstring = function (s) {
     let maxLen = 0;
-    let l = 0, r = 0;
-    let alphSet = new Set();
+    let l = 0, r=0;
+    let set = new Set();
     let len = s.length;
 
-    while (r < len) {
-        while (alphSet.has(s[r])) {
-            alphSet.delete(s[l]);
+    while(r<len){
+        while(set.has(s[r])){
+            set.delete(s[l]);
             l++;
         }
 
-        alphSet.add(s[r]);
-        maxLen = Math.max(maxLen, r - l + 1);
+        set.add(s[r]);
+        maxLen = Math.max(maxLen,(r-l+1));
         r++;
-
     }
 
     return maxLen;
