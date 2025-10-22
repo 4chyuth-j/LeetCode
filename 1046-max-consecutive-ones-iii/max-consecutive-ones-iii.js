a5a6a71620617b7f232f7486d53c9adcc277926a
Achyuth J. <achyuthj2002@gmail.com>
2025-10-22 09:49:10 +0530

    Time: 2 ms (89.20%) | Memory: 61.9 MB (45.75%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -5,23 +5,25 @@
  */
 var longestOnes = function(nums, k) {
     let maxlen = 0;
-    // let zeroCount = 0;
-    // let l=0, r=0;
+    let zeroCount = 0;
+    let l=0, r=0;
     let len = nums.length;
-    for(let i=0; i<len; i++){
-        let zeroes = 0;
-        for(let j=i; j<len; j++){
-            if(nums[j]==0){
-                zeroes++;
-            }
-
-            if(zeroes<=k){
-                maxlen = Math.max(maxlen, j-i+1);
-            } else {
-                break;
-            }
-
+    while(r<len){
+        if(nums[r]==0){
+            zeroCount++
         }
+        if(zeroCount<=k){
+            maxlen = Math.max(maxlen,r-l+1);
+        } else {
+            while(zeroCount>k){
+                if(nums[l]==0){
+                  zeroCount--;
+                }
+                l++;
+            }
+        }
+
+        r++;
     }
     
     return maxlen;
