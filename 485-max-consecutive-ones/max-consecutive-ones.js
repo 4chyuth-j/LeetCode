a7f8a20f6912b067ce09367c9f9d966f1dfebeb9
Achyuth J. <achyuthj2002@gmail.com>
2025-10-23 10:14:27 +0530

    Time: 3 ms (33.99%) | Memory: 60 MB (55.71%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -2,18 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
-    let l=0,r=0;
+var findMaxConsecutiveOnes = function (nums) {
+    let l = 0, r = 0;
     let maxCount = 0;
     let len = nums.length;
-    while(r<len){
+    while (r < len) {
 
-        if(nums[r]==0){
-            l = r+1;
-        }
-
-        if(nums[r]==1){
-            maxCount = Math.max(maxCount,r-l+1);
+        if (nums[r] == 0) {
+            l = r + 1;
+        } else {
+            maxCount = Math.max(maxCount, r - l + 1);
         }
         r++;
     }
