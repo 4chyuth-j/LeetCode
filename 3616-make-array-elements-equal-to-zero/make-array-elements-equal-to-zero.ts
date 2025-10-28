6f4452293fd7b564bc5a905f0e28938f80e7e652
Achyuth J. <achyuthj2002@gmail.com>
2025-10-28 10:14:01 +0530

    Time: 190 ms (23.53%) | Memory: 58.2 MB (29.41%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.ts
@@ -0,0 +1,48 @@
+function countValidSelections(nums: number[]): number {
+    let res = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]==0){
+            if(isValid(i,[...nums],true)){
+                res++;
+            }
+            if(isValid(i,[...nums],false)){
+                res++;
+            }
+        }
+    }
+
+    return res;
+};
+
+
+function isValid(ind:number,arr:number[],moveL:boolean):boolean{
+    // let moveL = false;
+    let n = arr.length;
+
+    while(ind>=0 && ind<n){
+
+        if(arr[ind]===0){
+            if(moveL){
+                ind--;
+            } else {
+                ind++
+            }
+        } else {
+            arr[ind]--;
+            moveL = !moveL;
+            if(moveL){
+                ind--;
+            } else {
+                ind++
+            }
+        }
+    }
+
+    for(let i=0; i<n; i++){
+        if(arr[i]!=0) {
+            return false;
+        }
+    }
+
+    return true;
+}
