74ce4ff3b0e4602a3e17c68a51754cab6c3e2113
Achyuth J. <achyuthj2002@gmail.com>
2025-10-28 10:15:26 +0530

    Time: 197 ms (17.65%) | Memory: 58.3 MB (29.41%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.ts
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.ts
@@ -1,11 +1,11 @@
 function countValidSelections(nums: number[]): number {
     let res = 0;
-    for(let i=0; i<nums.length; i++){
-        if(nums[i]==0){
-            if(isValid(i,[...nums],true)){
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            if (isValid(i, [...nums], true)) {
                 res++;
             }
-            if(isValid(i,[...nums],false)){
+            if (isValid(i, [...nums], false)) {
                 res++;
             }
         }
@@ -15,31 +15,26 @@ function countValidSelections(nums: number[]): number {
 };
 
 
-function isValid(ind:number,arr:number[],moveL:boolean):boolean{
-    // let moveL = false;
+function isValid(ind: number, arr: number[], moveL: boolean): boolean {
+
     let n = arr.length;
 
-    while(ind>=0 && ind<n){
+    while (ind >= 0 && ind < n) {
 
-        if(arr[ind]===0){
-            if(moveL){
-                ind--;
-            } else {
-                ind++
-            }
-        } else {
+        if (arr[ind] > 0) {
+
             arr[ind]--;
             moveL = !moveL;
-            if(moveL){
-                ind--;
-            } else {
-                ind++
-            }
+        }
+        if (moveL) {
+            ind--;
+        } else {
+            ind++
         }
     }
 
-    for(let i=0; i<n; i++){
-        if(arr[i]!=0) {
+    for (let i = 0; i < n; i++) {
+        if (arr[i] != 0) {
             return false;
         }
     }
