c57c65bcc8d2a434b4562c84aa41ca9e69943382
Achyuth J. <achyuthj2002@gmail.com>
2025-10-29 09:20:38 +0530

    Time: 2 ms (57.64%) | Memory: 58.4 MB (23.15%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.js
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.js
@@ -11,22 +11,31 @@ var applyOperations = function(nums) {
        }
    } 
 
-   moveZerosToEnd(nums,nums.length);
+//    moveZerosToEnd(nums,nums.length);
+
+    let j=0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]!=0 && nums[j]==0){
+            [nums[i],nums[j]] = [nums[j],nums[i]];
+        }
+        if(nums[j]!=0) j++;
+    }
+
    return nums;
 };
 
-function moveZerosToEnd(arr, n)
-{
-    let j=0, temp, i;
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]!=0 && arr[j]==0)
-            {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        if(arr[j]!=0)
-            j+=1;
-    }
-}
+// function moveZerosToEnd(arr, n)
+// {
+//     let j=0, temp, i;
+//     for(i=0;i<n;i++)
+//     {
+//         if(arr[i]!=0 && arr[j]==0)
+//             {
+//                 let temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         if(arr[j]!=0)
+//             j+=1;
+//     }
+// }
