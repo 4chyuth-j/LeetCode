bff99bf076cd7881889b8ac548d88becea5ef17b
Achyuth J. <achyuthj2002@gmail.com>
2025-10-29 09:21:04 +0530

    Time: 1 ms (82.76%) | Memory: 58.5 MB (14.78%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.js
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.js
@@ -3,7 +3,6 @@
  * @return {number[]}
  */
 var applyOperations = function(nums) {
-   let set = new Set();
    for(let i=0; i<nums.length-1; i++){
        if(nums[i]==nums[i+1]){
             nums[i]*=2;
