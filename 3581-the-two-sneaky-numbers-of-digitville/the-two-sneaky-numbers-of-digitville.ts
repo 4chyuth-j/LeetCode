c839dd54a6ed68be6efdebe50b3461d023ce797d
Achyuth J. <achyuthj2002@gmail.com>
2025-10-31 07:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (86.81%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.ts
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.ts
@@ -1,14 +1,14 @@
 function getSneakyNumbers(nums: number[]): number[] {
-    let freqMap = new Map();
-    for(let i=0; i<nums.length; i++){
-        freqMap.set(nums[i],(freqMap.get(nums[i])||0)+1);
-    }
+    let freqSet = new Set();
     let res = [];
-
-    for(let [key,val] of freqMap){
-        if(val>=2){
-            res.push(key);
-            if(res.length==2) return res;
+    for(let i=0; i<nums.length; i++){
+        if(freqSet.has(nums[i])){
+            res.push(nums[i]);
+        } else {
+            freqSet.add(nums[i]);
         }
     }
+
+    return res;
+    
 };
