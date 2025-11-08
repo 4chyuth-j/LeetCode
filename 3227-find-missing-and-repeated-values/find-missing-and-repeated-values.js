f5a632d0138eba6e4fe8c8d05a71fb5248cf37bf
Achyuth J. <achyuthj2002@gmail.com>
2025-11-08 13:12:32 +0530

    Time: 1 ms (96.40%) | Memory: 56.1 MB (94.60%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.js
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.js
@@ -2,26 +2,26 @@
  * @param {number[][]} grid
  * @return {number[]}
  */
-var findMissingAndRepeatedValues = function(grid) {
+var findMissingAndRepeatedValues = function (grid) {
     let n = grid.length;
-    let arr = new Array(n*n+1).fill(0);
+    let arr = new Array(n * n + 1).fill(0);
     let res = [];
-    outter:for(let i=0; i<n; i++){
-        for(let j=0; j<n; j++){
-            if(arr[grid[i][j]]==0){
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (arr[grid[i][j]] == 0) {
                 arr[grid[i][j]]++;
             } else {
                 res[0] = grid[i][j];
-                
+
             }
         }
     }
 
-    for(let i=1; i<arr.length; i++){
-        if(arr[i]==0){
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] == 0) {
             res.push(i);
             break;
-        }  
+        }
     }
 
     return res;
