4bb2f5dc1936e0269533240747d2b8434edbf68a
Achyuth J. <achyuthj2002@gmail.com>
2025-11-16 11:02:23 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (14.76%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let pasc = [[1]];
+    if(rowIndex==0){
+        return pasc[0]
+    }
+
+    for(let i=1;i<=rowIndex; i++){
+        let dum = [0,...pasc[i-1],0];
+        let res = [];
+        for(let j=0; j<pasc[i-1].length+1; j++){
+            res[j] = dum[j]+dum[j+1];
+        }
+        pasc.push(res);
+    }
+    return pasc[rowIndex];
+};
