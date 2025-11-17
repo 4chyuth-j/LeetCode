635eec9737746f8ef76142eadcac7cc111d7f30a
Achyuth J. <achyuthj2002@gmail.com>
2025-11-17 08:41:20 +0530

    Time: 1 ms (64.62%) | Memory: 61.1 MB (67.69%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var kLengthApart = function(nums, k) {
+    let count = 0;
+    let oneCount = 0
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]==0){
+            count++;
+        } else {
+            if(i==0 && nums[i]==1){ 
+                oneCount++;
+                continue;
+            }
+
+            if(oneCount==0){
+                count = 0;
+                oneCount++;
+                continue;
+            }
+
+            oneCount++;
+
+            if(count<k){
+                return false;
+            } else {
+                count = 0;
+            }
+        }
+    }
+
+    return true;
+};
