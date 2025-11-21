5b4d59100be3fd5ba703cc6648d47ecaa89728cc
Achyuth J. <achyuthj2002@gmail.com>
2025-11-21 08:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (10.56%) - LeetSync

---
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+//    let flag = new Array(k).fill(0);
+   let flag = new Set();
+
+   for(let i=1; i<=k; i++){
+        flag.add(i);
+   }
+
+   let res = 0;
+
+   while(flag.size){
+        let el = nums.pop();
+        if(flag.has(el)){
+            flag.delete(el);
+        }
+        res++;
+   }
+
+   return res;
+};
