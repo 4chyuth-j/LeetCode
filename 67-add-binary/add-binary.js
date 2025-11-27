4d90b1882e087c1b7486cd4160e600f9bb987494
Achyuth J. <achyuthj2002@gmail.com>
2025-11-27 08:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (71.34%) - LeetSync

--- 67-add-binary/add-binary.js
+++ 67-add-binary/add-binary.js
@@ -3,30 +3,26 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function(a, b) {
-    let maxLen = Math.max(a.length, b.length);
-    if(a.length<b.length){
-        let dum = new Array(maxLen-a.length).fill("0");
-        a = dum.join('')+a;
-    } else if (a.length>b.length){
-        let dum = new Array(maxLen-b.length).fill("0");
-        b = dum.join('')+b;
-    }
+var addBinary = function(a, b) { 
     let carry = 0;
     let res = "";
 
-    for(let i=maxLen-1; i>=0; i--){
-        let digitA = Number(a[i]) || 0;
-        let digitB = Number(b[i]) || 0;
-        let sum = digitA + digitB + carry;
-        let char = sum%2;
-        res = char+res;
+    let i = a.length -1;
+    let j = b.length -1;
+
+    
+
+    while(i>=0 || j>=0 || carry){
+        let sum = carry;
+        if(i>=0) sum+=Number(a[i]);
+        if(j>=0) sum+=Number(b[j]);
+        res = (sum%2) + res;
         carry = Math.floor(sum/2);
+        i--;
+        j--;
     }
 
-    if(carry){
-        res = carry + res;
-    }
+    
 
     return res;
 };
