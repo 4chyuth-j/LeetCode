683203cdfb71d27de0945c4b0c84704bc8d24e7a
Achyuth J. <achyuthj2002@gmail.com>
2025-12-01 08:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (54.96%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -6,10 +6,7 @@
 var intersection = function(nums1, nums2) {
     let set = new Set(nums1);
     let res = [];
-    // for(let i=0; i<nums1.length; i++){
-    //     set.add(nums1[i]);
-    // }
-
+    
     for(let i=0; i<nums2.length; i++){
         if(set.has(nums2[i])){
             res.push(nums2[i]);
