719884eba358b463f0a5ec49f81d680cebc4eb93
Achyuth J. <achyuthj2002@gmail.com>
2025-12-04 08:57:27 +0530

    Time: 2 ms (31.82%) | Memory: 59.3 MB (17.61%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function (items, ruleKey, ruleValue) {
+    let count = 0;
+    for (let i = 0; i < items.length; i++) {
+        if((ruleKey == 'type' && ruleValue == items[i][0]) || (ruleKey == "color" && ruleValue == items[i][1]) || (ruleKey == 'name' && ruleValue == items[i][2])){
+            count++;
+        }
+    }
+
+    return count;
+};
