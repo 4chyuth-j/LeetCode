b6c4d7847100e08caa7b5d919e9e73e1d97f8628
Achyuth J. <achyuthj2002@gmail.com>
2025-12-15 10:58:20 +0530

    Time: 11 ms (8.73%) | Memory: 59 MB (9.72%) - LeetSync

--- 389-find-the-difference/find-the-difference.js
+++ 389-find-the-difference/find-the-difference.js
@@ -4,22 +4,10 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let sMap = new Map();
-    
     for(let c of s){
-        sMap.set(c, (sMap.get(c) || 0)+1 );
+        t = t.replace(c,"")
     }
 
-    for(let c of t){
-        if(sMap.has(c)){
-            sMap.set(c,(sMap.get(c))-1);
-            if(sMap.get(c)==0){
-                sMap.delete(c);
-            }
-        } else {
-            return c;
-        }
-    }
-
+    return t;
     
 };
