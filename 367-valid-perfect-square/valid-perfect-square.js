7154c8c56e7da02c80f6fa2297330b597e4501cb
Achyuth J. <achyuthj2002@gmail.com>
2025-12-16 14:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (55.11%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.js
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -3,11 +3,18 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    if(num==1) return true;
-    for(let i=1; i<=num/2; i++){
-        if(i*i==num){
-            return true;
+    if(num<2) return true;
+    let left = 2, right = Math.floor(num/2);
+    while(left<=right){
+        const mid = Math.floor((left+right)/2);
+        const squred = mid*mid;
+        if(squred==num) return true;
+        else if (squred<num){
+            left = mid+1;
+        } else {
+            right = mid-1;
         }
     }
+
     return false;
 };
