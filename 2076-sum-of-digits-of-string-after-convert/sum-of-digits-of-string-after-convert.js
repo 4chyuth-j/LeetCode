cdcd753e60203d44bc0e9ccd779ad7e9d9bbae57
Achyuth J. <achyuthj2002@gmail.com>
2025-12-16 14:43:35 +0530

    Time: 13 ms (6.72%) | Memory: 58.5 MB (5.88%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.js
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.js
@@ -3,31 +3,26 @@
  * @param {number} k
  * @return {number}
  */
-var getLucky = function (s, k) {
-    const alpha = "abcdefghijklmnopqrstuvwxyz";
-    let lookup = new Map();
-    for(let i=0; i<alpha.length; i++){
-        lookup.set(alpha[i],i+1);
+var getLucky = function(s, k) {
+    // console.log("a".charCodeAt(0)-96)
+    let num = "";
+    // let sum = 0;
+    for(let c of s){
+        num+=(c.charCodeAt(0)-96);
+    }
+    // console.log(num);
+
+    while(k>0){
+        // let temp="";
+        let tempSum = 0;
+       for(let char of num){
+             tempSum +=Number(char);
+       }
+       console.log(tempSum);
+       num=""+tempSum;
+       k--;
     }
 
-    let alpDig = '';
-    for(let i=0; i<s.length; i++){
-        let value = lookup.get(s[i]);
-        alpDig +=`${value}`;
-    }
-
-    
-
-    let result = (alpDig);
-    for(let i=1; i<=k; i++){
-       let sum = 0;
-        for(let ch of result) {
-            sum += parseInt(ch);
-        }
-        result = sum.toString();
-    }
-
-    
-    return parseInt(result);
-
+    // console.log(num);
+    return Number(num);
 };
