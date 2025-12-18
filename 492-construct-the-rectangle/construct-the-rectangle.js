8b8de44fec4ab7c4fdad5f7c65ae35d9917a13c1
Achyuth J. <achyuthj2002@gmail.com>
2025-12-18 11:25:06 +0530

    Time: 3 ms (16.82%) | Memory: 54.7 MB (29.91%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.js
+++ 492-construct-the-rectangle/construct-the-rectangle.js
@@ -3,10 +3,25 @@
  * @return {number[]}
  */
 var constructRectangle = function(area) {
-    let width = Math.sqrt(area) | 0;
-    while(area%width!==0){
-        width--;
+     let map = new Map();
+    let bench = Math.sqrt(area);
+    let w = 1;
+    while(w<=bench){
+        let l = area/w;
+        if(l%1==0){
+            map.set(l-w,[l,w]);
+        }
+        w++;
     }
 
-    return [area/width,width];
+    let min = Infinity;
+
+    for(let key of map.keys()){
+        if(key<=min){
+            min = key;
+        }
+    }
+
+    return map.get(min);
+
 };
