fddfb000c0888b197d4b3164d065042db360727b
Achyuth J. <achyuthj2002@gmail.com>
2025-12-22 09:41:28 +0530

    Time: 3 ms (1.58%) | Memory: 54.3 MB (30.70%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    let map = new Map();
+    let curr = null;
+    let refKey = null;
+    
+    for(let num of nums){
+        if(num>curr && (num-1)==curr){
+            map.set(refkey,num);
+        } else {
+            map.set(num,num);
+            refkey = num;
+        }
+            curr = num;
+    }
+
+    console.log(map);
+
+    let res = [];
+
+    for(let [key,val] of map){
+        if(key==val){
+            res.push(""+key);
+        } else {
+            res.push(key+"->"+val);
+        }
+    }
+    return res;
+};
