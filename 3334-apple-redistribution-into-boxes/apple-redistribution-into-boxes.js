27455fbcde8412c7ccf661ee82e1812d9b5de8eb
Achyuth J. <achyuthj2002@gmail.com>
2025-12-24 09:20:25 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (58.18%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.js
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.js
@@ -7,14 +7,8 @@ var minimumBoxes = function(apple, capacity) {
     let totalApple = apple.reduce((curr,item)=>curr+item,0);
     capacity.sort((a,b)=>b-a);
     let res = 0;
-    let cap = 0;
-    for(let i=0; i<capacity.length; i++){
-
-        cap+=capacity[i];
-        res++;
-
-        if(cap>=totalApple) break;
+    while(totalApple>0){
+        totalApple-=capacity[res++];
     }
-
     return res;
 };
