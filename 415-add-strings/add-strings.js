313ca809da0bd5d4d9138efd8336c806d3a44c96
Achyuth J. <achyuthj2002@gmail.com>
2025-12-25 09:46:13 +0530

    Time: 5 ms (30.60%) | Memory: 58.5 MB (6.02%) - LeetSync

---
+++ 415-add-strings/add-strings.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    // console.log("9".charCodeAt(0)-48);
+    let res = '';
+    let carry = 0;
+    let i = num1.length-1;
+    let j = num2.length-1;
+
+    while(i>=0 || j>=0 ){
+        let dig1 =num1[i]? num1[i].charCodeAt(0)-48 : 0;
+        let dig2 =num2[j]? num2[j].charCodeAt(0)-48 : 0;
+        let sum = dig1+dig2+carry;
+        carry = 0;
+        if(sum>9){
+            carry = Math.floor(sum/10);
+            sum = sum%10;
+        }
+        res = sum +res;
+        i--;
+        j--;
+
+    }
+
+
+
+    return carry? carry+res: res;
+};
