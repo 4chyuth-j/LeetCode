5a18ddbfa75cdcdd901feadc4c99750027ec4206
Achyuth J. <achyuthj2002@gmail.com>
2026-01-03 09:45:42 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (7.30%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,23 +3,12 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    
-    // let num = BigInt(digits.join(""));  // Convert array to a BigInt
-    // num = num + BigInt(1);
-    // num=num.toString();
-    // num =num.split("")
-    // for(let i=0; i<num.length; i++){
-    //    num[i] = parseInt(num[i]);
-    // }
-
-    // return num;
 
     let carry = 1;
 
     let ind = digits.length -1;
     while(carry!=0 && ind>=0){
-        let sum = carry;
-        sum += digits[ind];
+        const sum = carry+digits[ind];
         digits[ind] = sum%10;
         carry = Math.floor(sum/10);
         ind--;
