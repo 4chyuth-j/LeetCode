9e59a568901ed991b8c2d5ba4f3502be9815531b
Achyuth J. <achyuthj2002@gmail.com>
2026-01-06 11:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (96.11%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -1,13 +1,33 @@
 /**
- * @param {number[]} nums
+ * @param {number[]} arr
  * @param {number} target
  * @return {boolean}
  */
-var search = function(nums, target) {
-    
-    for(let num of nums){
-        if(num===target) return true;
+var search = function(arr, target) {
+    let start = 0, end = arr.length-1;
+
+    while(start<=end){
+        let mid = Math.floor(start+(end-start)/2);
+        if(arr[mid]===target) return true;
+        if(arr[start]===arr[mid] && arr[mid]===arr[end]){
+            start = start+1;
+            end = end-1;
+            continue;
+        } else if(arr[mid]<=arr[end]){
+            if(arr[mid]<target && target<=arr[end]){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        } else {
+            if(arr[start]<=target && target<arr[mid]){
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
     }
 
     return false;
+    
 };
