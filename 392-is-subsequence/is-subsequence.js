a633ddb1fdd29c8893568f18c449abac01a24ad6
Achyuth J. <achyuthj2002@gmail.com>
2026-01-09 09:44:29 +0530

    Time: 1 ms (72.62%) | Memory: 54.9 MB (41.81%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -3,17 +3,33 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function(s, t) {
-    let stack = [];
-    for(let i=s.length-1; i>=0; i--){
-        stack.push(s[i]);
-    }
+var isSubsequence = function (s, t) {
 
-    for(let c of t){
-        if(c===stack[stack.length-1]){
-            stack.pop();
+    let sp=0;
+    let tp=0;
+
+    while(sp<s.length && tp<t.length){
+        if(s[sp]==t[tp]){
+            sp++;
         }
+
+        tp++;
     }
 
-    return stack.length===0;
+    return sp===s.length;
+
 };
+
+
+// let stack = [];
+// for (let i = s.length - 1; i >= 0; i--) {
+//     stack.push(s[i]);
+// }
+
+// for (let c of t) {
+//     if (c === stack[stack.length - 1]) {
+//         stack.pop();
+//     }
+// }
+
+// return stack.length === 0;
