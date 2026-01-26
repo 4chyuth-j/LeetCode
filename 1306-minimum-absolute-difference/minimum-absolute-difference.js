a3d2b0f9f8446fad7736d7453e4a70802a9b8c65
Achyuth J. <achyuthj2002@gmail.com>
2026-01-26 10:57:12 +0530

    Time: 53 ms (82.01%) | Memory: 70.1 MB (56.40%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.js
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.js
@@ -4,23 +4,19 @@
  */
 var minimumAbsDifference = function(arr) {
     arr.sort((a,b)=>a-b);
-    let map = new Map();
     let min = Infinity;
+    let res = [];
     for(let i=0; i<arr.length-1; i++){
         let diff = arr[i+1]-arr[i];
-        if(diff<=min){
-          let val = map.get(diff) || [];
-          val.push([arr[i],arr[i+1]])
-          map.set(diff,val);
+        if(diff<min){
+          min = diff;
+          res = [i];
+        } else if(diff==min) {
+            res.push(i);
         }
-        min = Math.min(min,diff);
+        
     }
 
-    for(let [key,val] of map){
-        if(key==min){
-            return val;
-        }
-    }
-
+    return res.map(ind=>[arr[ind],arr[ind+1]]);
 
 };
