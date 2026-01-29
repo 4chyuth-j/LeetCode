4f622afad0ff264a4067ae151e75e6e2c744a1fe
Achyuth J. <achyuthj2002@gmail.com>
2026-01-29 10:36:04 +0530

    Time: 2 ms (20.03%) | Memory: 58.5 MB (37.15%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -13,17 +13,8 @@
 var maxDepth = function(root) {
     if(!root) return 0;
 
-    let queue = [root];
-    let depth = 0;
-    while(queue.length){
-        depth++;
-        let len = queue.length;
-        for(let i=0; i<len; i++){
-            let elem = queue.shift();
-            if(elem.left) queue.push(elem.left);
-            if(elem.right) queue.push(elem.right);
-        }
-    }
+    let lh = maxDepth(root.left);
+    let rh = maxDepth(root.right);
 
-    return depth;
+    return 1 + Math.max(lh,rh);
 };
