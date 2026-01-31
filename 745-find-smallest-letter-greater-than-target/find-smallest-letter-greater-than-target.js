678928c898c65a24860f8a8b90f1194037a7f550
Achyuth J. <achyuthj2002@gmail.com>
2026-01-31 09:36:45 +0530

    Time: 2 ms (9.80%) | Memory: 55.2 MB (54.66%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    for(let c of letters){
+        if(c>target){
+            return c;
+        }
+    }
+
+    return letters[0];
+};
