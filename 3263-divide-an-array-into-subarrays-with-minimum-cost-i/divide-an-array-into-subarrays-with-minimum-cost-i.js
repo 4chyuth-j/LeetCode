eea19bc93a2c18ea4c5db5cf8881a425485c5564
Achyuth J. <achyuthj2002@gmail.com>
2026-02-01 13:09:24 +0530

    Time: 1 ms (88.46%) | Memory: 55.7 MB (84.62%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.js
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var minimumCost = function(nums) {
-    // if(nums.length==3) return nums[0]+nums[1]+nums[2]
-    // let first = nums[0];
-    // nums.sort((a,b)=>a-b);
-    // console.log(nums);
-    // if(nums[0]!=first){
-    //     return first +nums[0]+nums[1];
-    // } else {
-    //     return nums[0]+nums[1]+nums[2];
-    // }
+    if(nums.length===3) return nums[0]+nums[1]+nums[2];
 
-    let arr = nums.slice(1).sort((a,b)=>a-b);
-    console.log(arr);
+    let min1=Infinity, min2=Infinity;
+    for(let i=1; i<nums.length; i++){
+        if(nums[i]<min1){
+            min2 = min1;
+            min1 = nums[i];
+        } else if (nums[i]<min2){
+            min2 = nums[i];
+        }
+    }
 
-    return nums[0]+arr[0]+arr[1];
+    return nums[0]+min1+min2;
 };
