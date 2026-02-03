c842b5aa2f111ed840606ace6c51e7d58a0446d2
Achyuth J. <achyuthj2002@gmail.com>
2026-02-03 10:28:48 +0530

    Time: 1 ms (35.48%) | Memory: 55.2 MB (77.42%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.js
+++ 3952-trionic-array-i/trionic-array-i.js
@@ -3,27 +3,23 @@
  * @return {boolean}
  */
 var isTrionic = function(nums) {
-    if(nums.length==3) return false;
-    let flag = 0;
-    let inc = true;
-    let prev = nums[0]
-    for(let i=1; i<nums.length; i++){
-        if(nums[i]>prev && inc){
-            prev = nums[i];
-            if(i<nums.length-1 && nums[i]>nums[i+1]){
-                inc = !inc;
-                flag++;
-            }
-        } else if(nums[i]<prev && !inc){
-            prev = nums[i];
-            if(i<nums.length-1 && nums[i]<nums[i+1]){
-                inc = !inc;
-                flag++;
-            }
-        } else {
-            return false;
-        }
-    }
+    let n = nums.length;
+    let i = 0;
 
-    return flag==2;
+    //checking strictly increasing
+    while(i+1<n && nums[i]<nums[i+1]) i++;
+
+    if(i===0 || i===n-1) return false;
+
+    let peak = i;
+
+    //checking the second strictly decreasing
+    while(i+1<n && nums[i]>nums[i+1]) i++;
+
+    if(i===peak || i===n-1) return false;
+
+    //checking wheather the remaining elements is increasing till the end
+    while(i+1<n && nums[i]<nums[i+1]) i++;
+
+    return i===n-1;
 };
