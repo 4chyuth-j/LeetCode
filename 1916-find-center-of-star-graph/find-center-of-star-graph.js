80f06e894350400c67a7f8316e5ea06dc8a7a9f9
Achyuth J. <achyuthj2002@gmail.com>
2026-02-07 09:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 76.3 MB (83.19%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.js
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.js
@@ -3,23 +3,9 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    let freq = {};
-    for(let item of edges){
-        if(!freq[item[0]]) freq[item[0]] = 0;
-        if(!freq[item[1]]) freq[item[1]] = 0;
-
-        freq[item[0]]++;
-        freq[item[1]]++;
+    if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1]){
+        return edges[0][0];
+    } else {
+        return edges[0][1];
     }
-
-    let centre = -1;
-    let count = 0;
-    for(let node in freq){
-        if(freq[node]>count){
-            count = freq[node];
-            centre = Number(node);
-        }
-    }
-
-    return centre;
 };
