00bf54282406df88dd0331966e7b063427df8eb1
Achyuth J. <achyuthj2002@gmail.com>
2026-02-11 09:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (64.54%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.js
+++ 3312-number-of-changing-keys/number-of-changing-keys.js
@@ -4,8 +4,9 @@
  */
 var countKeyChanges = function(s) {
     let count =0;
+    s = s.toLowerCase();
     for(let i=0; i<s.length-1; i++){
-        if(s[i].toLowerCase()!==s[i+1].toLowerCase()) count++;
+        if(s[i]!==s[i+1]) count++;
     }
 
     return count;
