e09d845ee3669769a6475f62661374400f719e9e
Achyuth J. <achyuthj2002@gmail.com>
2026-02-24 12:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (45.22%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -4,18 +4,10 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    let start = 0,end = letters.length-1;
-    let res = letters[0];
-
-    while(start<=end){
-        let mid = Math.floor((start+end)/2);
-        if(letters[mid]>target){
-            res = letters[mid];
-            end = mid-1;
-        } else {
-            start = mid+1;
-        }
+    let res = [];
+    for(let c of letters){
+        if(c>target) res.push(c);
     }
-    return res;
-};
 
+    return res.length!==0?res[0]:letters[0];
+};
