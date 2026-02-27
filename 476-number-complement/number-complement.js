9e1b4fe80366e034aa0e60de49741c3383b1e67d
Achyuth J. <achyuthj2002@gmail.com>
2026-02-27 08:47:11 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (59.09%) - LeetSync

---
+++ 476-number-complement/number-complement.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let bin = num.toString(2).split("");
+    for(let i=0; i<bin.length; i++){
+        if(bin[i]==='1'){
+            bin[i]=0;
+        } else {
+            bin[i]=1;
+        }
+    }
+
+    return parseInt(bin.join(''),2);
+};
