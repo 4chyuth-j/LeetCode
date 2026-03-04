e7da7d5dae2c98b8e5214d92e4f7f21300020ec8
Achyuth J. <achyuthj2002@gmail.com>
2026-03-04 11:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (65.96%) - LeetSync

---
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minTimeToType = function(word) {
+    let ans = 0;
+    let pointer = 'a';
+    for(let c of word){
+        let clockwise  = Math.abs(c.charCodeAt(0)-pointer.charCodeAt(0));
+        let counterClockwise = 26-clockwise;
+        ans+=Math.min(clockwise,counterClockwise);
+        pointer = c;
+    }
+
+    return ans+word.length;
+};
