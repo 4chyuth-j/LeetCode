3dc1c4f3430384d888bacc8fac81787ceb8a439e
Achyuth J. <achyuthj2002@gmail.com>
2026-03-05 09:32:21 +0530

    Time: 4 ms (30.43%) | Memory: 54.1 MB (58.70%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -3,28 +3,19 @@
  * @return {number}
  */
 var minOperations = function(s) {
-    let str1 = '';
-    let start = 0;
-    for(let i=0; i<s.length; i++){
-        str1+=start;
-        start = start==0?1:0;
-    }
+    
 
-    start = 1;
-    let str2 = '';
-    for(let i=0; i<s.length; i++){
-        str2+=start;
-        start = start==0?1:0;
-    }
+    function getChange(start){
+        let count=0;
+        for(let i=0; i<s.length; i++ ){
+            if(s[i]!=start)count++;
 
+            start = start===0?1:0;
+        }
 
+        return count;
+    }   
 
-    let diff1=0,diff2 =0;
-
-    for(let i=0; i<s.length; i++){
-        if(str1[i]!=s[i]) diff1++;
-        if(str2[i]!=s[i]) diff2++;
-    }
-
-    return Math.min(diff1,diff2);
+    return Math.min(getChange(0),getChange(1));
 };
+
