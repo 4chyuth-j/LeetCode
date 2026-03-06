2ce26c48564a50bc319024a97d1346e6cfb5221a
Achyuth J. <achyuthj2002@gmail.com>
2026-03-06 09:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (59.52%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -3,8 +3,6 @@
  * @return {boolean}
  */
 var checkOnesSegment = function(s) {
-    let seg = 0 ;
-    let len = 0;
     for(let i=0; i<s.length-1; i++ ){
         if(s[i]==0 && s[i+1]=='1') return false;
     }
