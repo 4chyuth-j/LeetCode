a35a098a599adf0d7f3c05dc917d54d0d681cf26
Achyuth J. <achyuthj2002@gmail.com>
2026-03-07 09:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (68.12%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
@@ -3,18 +3,8 @@
  * @return {boolean}
  */
 var squareIsWhite = function(coordinates) {
-    let map = {
-        'a':1,
-        'b':2,
-        'c':3,
-        'd':4,
-        'e':5,
-        'f':6,
-        'g':7,
-        'h':8,
-    };
 
-    let getalp = map[coordinates[0]];
+    let getalp = coordinates.charCodeAt(0)-96;
     if(getalp%2===1){
         if(coordinates[1]%2===0) return true;
     } else {
