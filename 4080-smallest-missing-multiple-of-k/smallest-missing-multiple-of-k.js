74aac7ae703f69ab1f21915ab175852d9dc2a489
Achyuth J. <achyuthj2002@gmail.com>
2026-03-14 08:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (80.28%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.js
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.js
@@ -4,19 +4,6 @@
  * @return {number}
  */
 var missingMultiple = function(nums, k) {
-    let len = nums.length;
-    let freq = {};
-    for(let i=1; i<=len+1; i++){
-        freq[k*i] = 0;
-    }
-
-    for(let i=0; i<len; i++){
-        if(nums[i]%k===0) {
-            freq[nums[i]]++
-        }
-    }
-
-    for(let key in freq){
-        if(freq[key]===0) return Number(key);
-    }
+    for (let i = k; ; i += k)
+        if (!nums.includes(i)) return i;
 };
