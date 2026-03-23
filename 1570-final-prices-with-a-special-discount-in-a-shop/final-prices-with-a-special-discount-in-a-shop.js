8de27db478d0c75bf6de6e3fdf810117cd754ca5
Achyuth J. <achyuthj2002@gmail.com>
2026-03-23 09:02:21 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (99.41%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
@@ -6,14 +6,11 @@ var finalPrices = function(prices) {
    for(let i=0; i<prices.length ;i++){
         let curr = prices[i];
         let d = i+1;
-        let discount = curr;
         while(d<prices.length){
-            if(discount>=prices[d]){
-                discount = prices[d];
-                prices[i] = curr - discount;
+            if(curr>=prices[d]){
+                prices[i] = curr - prices[d];
                 break;
             } 
-
             d++;
         }
    } 
