a574e36175bf65900cf117818109439ed1ee9a72
Achyuth J. <achyuthj2002@gmail.com>
2026-03-29 10:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 56.6 MB (42.86%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.js
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.js
@@ -7,33 +7,20 @@ var canBeEqual = function(s1, s2) {
     if(s1===s2) return true;
 
     let oddMap = new Map();
-    for(let i=0; i<s1.length; i=i+2){
-        oddMap.set(s1[i],(oddMap.get(s1[i]) || 0)+1);
-    }
-
-    for(let i=0; i<s2.length; i=i+2){
-        if(oddMap.has(s2[i])){
-            let count = oddMap.get(s2[i])-1;
-            oddMap.set(s2[i],count);
-            if(count==0) oddMap.delete(s2[i]);
-        } else {
-            return false;
-        }
-    }
-
     let evenMap = new Map();
-    for(let i=1; i<s1.length; i=i+2){
-        evenMap.set(s1[i],(evenMap.get(s1[i]) || 0)+1);
+
+    for(let i=0; i<s1.length; i++){
+        let map = (i%2===0)?evenMap:oddMap;
+        map.set(s1[i],(map.get(s1[i]) || 0)+1);
     }
 
-    for(let i=1; i<s2.length; i=i+2){
-        if(evenMap.has(s2[i])){
-            let count = evenMap.get(s2[i])-1;
-            evenMap.set(s2[i],count);
-            if(count==0) evenMap.delete(s2[i]);
-        } else {
-            return false;
-        }
+    for(let i=0; i<s2.length; i++){
+        let map = (i%2===0)?evenMap:oddMap;
+        if(!map.has(s2[i])) return false;
+
+        let count = map.get(s2[i])-1;
+        map.set(s2[i],count);
+        if(count==0) map.delete(s2[i]);
     }
 
     return true;
