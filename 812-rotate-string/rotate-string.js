f6fb47f26e620bc4f299f6a5fde74e4eb74ccd73
Achyuth J. <achyuthj2002@gmail.com>
2026-04-02 21:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (67.74%) - LeetSync

--- 812-rotate-string/rotate-string.js
+++ 812-rotate-string/rotate-string.js
@@ -4,16 +4,7 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-    if(s==goal) return true;
+    if(s.length!==goal.length) return false;
 
-    let word = s.split("");
-    let n = s.length;
-
-    for(let i=0; i<n; i++){
-        let char = word.shift();
-        word.push(char);
-        if(goal === word.join("")) return true;
-    }
-
-    return false
+    return (s+s).includes(goal);
 };
