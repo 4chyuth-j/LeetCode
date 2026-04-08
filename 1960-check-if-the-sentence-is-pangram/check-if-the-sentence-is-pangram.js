a6173843e798f63c0f75f7f0bbfffb73dbd22f24
Achyuth J. <achyuthj2002@gmail.com>
2026-04-08 09:46:45 +0530

    Time: 2 ms (66.13%) | Memory: 55.9 MB (22.04%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -3,18 +3,6 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let flag = new Array(26).fill(0);
-
-    console.log('a'.charCodeAt(0))
-
-    for(let c of sentence){
-        let code = c.charCodeAt(0)-97;
-        flag[code] = 1; 
-    }
-
-    for(let c of flag){
-        if(c==0) return false;
-    }
-
-    return true;
+    let set = new Set(sentence);
+    return set.size===26;
 };
