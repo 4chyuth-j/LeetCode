f4c1e70f454c7ae9dab6088fb49510f3035d55e4
Achyuth J. <achyuthj2002@gmail.com>
2026-04-23 10:52:34 +0530

    Time: 4 ms (11.76%) | Memory: 56.4 MB (62.74%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isGood = function(nums) {
+    let max = -Infinity;
+    for(let num of nums){
+        max = Math.max(max,num);
+    }
+
+    let res = new Array(max+1).fill(0);
+
+    for(let num of nums){
+        res[num]++;
+    }
+
+    if(res[max]!=2) return false;
+
+    for(let i=1; i<max; i++){
+        if(res[i]==0 || res[i]>1) return false;
+    }
+
+    return true;
+};
