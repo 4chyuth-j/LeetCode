d9b6872544567bafd30616f5c3ba32e0ec108e31
Achyuth J. <achyuthj2002@gmail.com>
2026-04-30 13:29:27 +0530

    Time: 20 ms (15.13%) | Memory: 64.3 MB (84.87%) - LeetSync

--- 1708-design-parking-system/design-parking-system.js
+++ 1708-design-parking-system/design-parking-system.js
@@ -4,9 +4,7 @@
  * @param {number} small
  */
 var ParkingSystem = function(big, medium, small) {
-    this.big = big;
-    this.medium = medium;
-    this.small = small;
+    this.slot = [big,medium,small];
 };
 
 /** 
@@ -14,28 +12,12 @@ var ParkingSystem = function(big, medium, small) {
  * @return {boolean}
  */
 ParkingSystem.prototype.addCar = function(carType) {
-    if(carType ===1){
-        if(this.big!=0){
-            this.big--;
-            return true;
-        } else {
-            return false;
-        }
-    } else if(carType ===2){
-        if(this.medium!=0){
-            this.medium--;
-            return true;
-        } else {
-            return false
-        }
-    } else {
-        if(this.small!=0){
-            this.small--;
-            return true;
-        } else {
-            return false;
-        }
+    if(this.slot[carType-1]>0){
+        this.slot[carType-1]--;
+        return true;
     }
+
+    return false;
 };
 
 /** 
