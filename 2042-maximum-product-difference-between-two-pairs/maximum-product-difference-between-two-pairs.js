a074f9360fce30193101e081330c42c4eb9e82a7
Achyuth J. <achyuthj2002@gmail.com>
2026-05-01 19:54:33 +0530

    Time: 58 ms (75.41%) | Memory: 58.9 MB (5.74%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
@@ -3,7 +3,24 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    nums.sort((a,b)=>a-b);
-    let l = nums.length-1;
-    return (nums[l]*nums[l-1]) - (nums[0]*nums[1]);
+    let max = -1,smax = -1;
+    let min = Infinity ,smin = Infinity ;
+
+    for(let num of nums){
+        if(num>max){
+            smax = max;
+            max = num;
+        } else if(num>smax){
+            smax = num;
+        }
+
+        if(num<min){
+            smin = min;
+            min = num;
+        } else if(num<smin){
+            smin = num;
+        }
+    }
+
+    return (max*smax) - (min*smin);
 };
