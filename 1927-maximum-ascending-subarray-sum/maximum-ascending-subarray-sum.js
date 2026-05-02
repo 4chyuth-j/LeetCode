ea0bb39a18c17662fb9c516ad34ef5b55414f191
Achyuth J. <achyuthj2002@gmail.com>
2026-05-02 09:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (8.73%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+    let max = nums[0];
+    let j = 0;
+    let sum = nums[0];
+    for(let i=1; i<nums.length; i++){
+       if(nums[i-1]>=nums[i]){
+            j=i;
+            sum = 0;
+       }
+
+       sum+=nums[i];
+       max = Math.max(max,sum);
+    }
+
+    return max;
+};
