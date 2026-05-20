adb7f43d3c4a3898fc697bca4341d3b8fb310482
Achyuth J. <achyuthj2002@gmail.com>
2026-05-20 21:24:05 +0530

    Time: 9 ms (95.30%) | Memory: 62.9 MB (57.05%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.js
+++ 2486-most-frequent-even-element/most-frequent-even-element.js
@@ -4,22 +4,20 @@
  */
 var mostFrequentEven = function(nums) {
     let freq = new Map();
+    let ans = -1;
+    let maxFreq = 0;
     for(let num of nums){
-        if(num%2===0){
-            freq.set(num,(freq.get(num)||0)+1);
+        if(num%2!==0) continue;
+
+        const count = (freq.get(num)||0)+1;
+        freq.set(num,count);
+        if(count>maxFreq || (count ==maxFreq && num<ans)){
+            ans = num;
+            maxFreq = count;
         }
+        
     }
 
-    let res = {key:-1,val:0};
+    return ans;
     
-    for(let [key,val] of freq){
-        if(res.val<val){
-            res.val = val;
-            res.key = key;
-        } else if(res.val===val && res.key>key){
-            res.key = key;
-        }
-    }
-
-    return res.key
 };
