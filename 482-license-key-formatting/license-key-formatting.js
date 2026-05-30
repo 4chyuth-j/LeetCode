2fa6713616a6854c225200e544da88106a521652
Achyuth J. <achyuthj2002@gmail.com>
2026-05-30 11:23:39 +0530

    Time: 2 ms (98.72%) | Memory: 58 MB (90.17%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.js
+++ 482-license-key-formatting/license-key-formatting.js
@@ -4,37 +4,15 @@
  * @return {string}
  */
 var licenseKeyFormatting = function(s, k) {
-    let str = s.split("-").join("");
+    let str = s.split("-").join("").toUpperCase();
     let len = str.length;
-    let res = '';
+    let firstGroup = len%k || k;
+    let res = str.slice(0,firstGroup);
 
-    for(let i=0; i<(len%k); i++){
-        if(/^[a-zA-Z]$/.test(str[i])){
-            res+=str[i].toUpperCase();
-        } else {
-            res+=str[i];
-        }
+    for(let i=firstGroup; i<len; i+=k){
+        res +="-" + str.slice(i,i+k);
     }
 
-    let i = len%k;
+    return res;
 
-    if(i!=0) res+='-';
-
-    while(i<len){
-        let count = k;
-        while(count){
-            if(/^[a-zA-Z]$/.test(str[i])){
-               res+=str[i].toUpperCase();
-            } else {
-               res+=str[i];
-            }
-            i++;
-            count--;
-        }
-
-        res+='-';
-        
-    }
-
-    return res.slice(0,res.length-1);
 };
