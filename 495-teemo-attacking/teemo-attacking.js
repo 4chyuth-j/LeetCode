56c3f172be0e6ca8853a2fb7c987cf3d653a2d15
Achyuth J. <achyuthj2002@gmail.com>
2026-06-04 08:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (76.40%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.js
+++ 495-teemo-attacking/teemo-attacking.js
@@ -5,25 +5,12 @@
  */
 var findPoisonedDuration = function(timeSeries, duration) {
     let time = 0;
-    
-    for(let i=0; i<timeSeries.length; i++ ){
-        let curr = timeSeries[i]+duration-1;
-        if(curr==timeSeries[i+1]){
-            time+=duration-1;
-        }else if(curr>timeSeries[i+1]){
-            time+=timeSeries[i+1]-timeSeries[i];
-        } else {
-            time+=duration;
-        } 
+
+    for(let i=0; i<timeSeries.length-1; i++ ){
+        let diff = timeSeries[i+1]-timeSeries[i];
+        time += diff>duration? duration: diff;
     }
 
-    return time;
+    return timeSeries.length? time+duration : time ;
 };
 
-/*
-if(curr<timeSeries[i+1]){
-            time+=duration;
-        } else if (curr==timeSeries[i+1]){
-            time+=duration-1;
-        } 
- */
