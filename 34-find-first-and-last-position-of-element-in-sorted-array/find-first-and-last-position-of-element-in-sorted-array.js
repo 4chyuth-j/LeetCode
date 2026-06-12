97c591d3fa74c0d964aa3d481ca4ad20425c0bc1
Achyuth J. <achyuthj2002@gmail.com>
2026-06-12 19:23:55 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (5.05%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,5 +4,46 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    return [nums.indexOf(target),nums.lastIndexOf(target)];
+    return [
+            findFirst(nums,target),
+            findLast(nums,target)
+            ];
 };
+
+function findFirst(arr,target){
+    let start = 0, end = arr.length-1;
+    let ans = -1;
+
+    while(start<=end){
+        const mid = Math.floor((start+end)/2);
+        if(arr[mid]===target){
+            ans = mid;
+            end = mid-1;
+        } else if(arr[mid]<target){
+            start = mid+1;
+        } else {
+            end = mid-1;
+        }
+    }
+
+    return ans;
+}
+
+function findLast(arr,target){
+    let start = 0, end = arr.length-1;
+    let ans = -1;
+
+    while(start<=end){
+        const mid = Math.floor((start+end)/2);
+        if(arr[mid]===target){
+            ans = mid;
+            start = mid+1;
+        } else if(arr[mid]<target){
+            start = mid+1;
+        } else {
+            end = mid-1;
+        }
+    }
+
+    return ans;
+}
