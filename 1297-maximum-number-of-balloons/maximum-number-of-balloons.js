424f66d085767e80e0cb84b32ac956ff953639fc
Achyuth J. <achyuthj2002@gmail.com>
2026-06-22 09:22:13 +0530

    Time: 7 ms (53.46%) | Memory: 58 MB (28.85%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.js
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.js
@@ -2,11 +2,11 @@
  * @param {string} text
  * @return {number}
  */
-var maxNumberOfBalloons = function(text) {
-   const map = { b: 0, a: 0, l: 0, o: 0, n: 0, };
-   for(let c of text){
-    map[c]++;
-   }
+var maxNumberOfBalloons = function (text) {
+    const map = { b: 0, a: 0, l: 0, o: 0, n: 0, };
+    for (let c of text) {
+        map[c]++;
+    }
 
-   return Math.floor(Math.min(map.b, map.a, map.l/2, map.o/2, map.n));
+    return Math.floor(Math.min(map.b, map.a, map.l / 2, map.o / 2, map.n));
 };
