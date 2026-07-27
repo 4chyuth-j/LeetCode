65017956b48a0277c5311e0950501de82ba2f6bf
Achyuth J. <achyuthj2002@gmail.com>
2026-07-27 09:02:30 +0530

    Time: 1 ms (77.68%) | Memory: 56.5 MB (12.88%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    // nums.sort((a,b)=>b-a);
-    // return (nums[0]-1)*(nums[1]-1);
+    
     let first = 0,second =0;
     for(let num of nums){
-        if(num>first){
+        if(num>=first){
             second = first;
             first = num;
         } else if(num>second){
