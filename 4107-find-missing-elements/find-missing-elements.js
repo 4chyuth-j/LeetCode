91115eedbf397c1f3ecd3029336b2b2e807ea78d
Achyuth J. <achyuthj2002@gmail.com>
2026-08-04 11:19:37 +0530

    Time: 5 ms (28.35%) | Memory: 59.7 MB (12.60%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.js
+++ 4107-find-missing-elements/find-missing-elements.js
@@ -2,26 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findMissingElements = function(nums) {
+var findMissingElements = function (nums) {
     let set = new Set(nums);
     let min = Infinity, max = -Infinity;
-    for(let n of nums){
 
-        if(n<min) min = n;
-        if(n>max) max = n;
-
+    for (let n of nums) {
+        if (n < min) min = n;
+        if (n > max) max = n;
     }
 
-    let i = min;
     let res = [];
 
-    while(i<=max){
-        if(!set.has(i)){
-          res.push(i)
+    while (min <= max) {
+        if (!set.has(min)) {
+            res.push(min)
         }
-        i++;
+        min++;
     }
 
     return res;
-
 };
