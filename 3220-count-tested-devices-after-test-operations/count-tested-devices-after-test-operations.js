7d903694a4cb61c4941e21fd08e26b6403550e72
Achyuth J. <achyuthj2002@gmail.com>
2026-08-12 08:49:29 +0530

    Time: 4 ms (4.00%) | Memory: 56.6 MB (24.00%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} batteryPercentages
+ * @return {number}
+ */
+var countTestedDevices = function(batteryPercentages) {
+    let count = 0;
+    for(let i=0; i<batteryPercentages.length; i++){
+        if(batteryPercentages[i]>0){
+            count++;
+            for(let j=i+1; j<batteryPercentages.length; j++){
+                batteryPercentages[j] = Math.max(0, batteryPercentages[j] - 1);
+            }
+        } 
+    }
+
+    return count;
+};
