b79d1581b12442cfc6972194995dbcba3aa6b7bf
Achyuth J. <achyuthj2002@gmail.com>
2026-08-13 12:37:26 +0530

    Time: 1 ms (100.00%) | Memory: 58.6 MB (42.86%) - LeetSync

--- 3164-last-visited-integers/last-visited-integers.js
+++ 3164-last-visited-integers/last-visited-integers.js
@@ -8,14 +8,14 @@ var lastVisitedIntegers = function(nums) {
     while(i<nums.length){
         let k = 0;
         if(nums[i]>0){
-            seen.unshift(nums[i]);
+            seen.push(nums[i]);
             i++;
         }
 
         while(nums[i]<0 && i<nums.length){
             k++;
-            if(seen[k-1]){
-                ans.push(seen[k-1])
+            if(k<=seen.length){
+                ans.push(seen[seen.length-k])
             } else {
                 ans.push(-1);
             }
