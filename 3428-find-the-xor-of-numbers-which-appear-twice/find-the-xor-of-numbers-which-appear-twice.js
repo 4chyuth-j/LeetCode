e4b66dc1d26078f49bc0994dd454740e5e6900ee
Achyuth J. <achyuthj2002@gmail.com>
2026-08-19 07:16:22 +0530

    Time: 4 ms (23.64%) | Memory: 58.3 MB (10.91%) - LeetSync

---
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var duplicateNumbersXOR = function(nums) {
+    let freq = new Map();
+    for(let num of nums){
+        freq.set(num,(freq.get(num)||0)+1);
+    }
+
+    let res = 0;
+    for(let [key,val] of freq){
+        if(val==2){
+          res^=Number(key);
+        }
+    }
+
+    return res;
+};
