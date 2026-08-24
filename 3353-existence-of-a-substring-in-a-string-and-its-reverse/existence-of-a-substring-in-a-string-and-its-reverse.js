a79530b9becce66f9074ba5bf20490be786091a7
Achyuth J. <achyuthj2002@gmail.com>
2026-08-24 08:10:21 +0530

    Time: 2 ms (65.79%) | Memory: 55.2 MB (84.21%) - LeetSync

--- 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.js
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.js
@@ -4,9 +4,15 @@
  */
 var isSubstringPresent = function(s) {
     let revString = s.split("").reverse().join("");
+    let hash = new Set();
     for(let i=0; i<s.length-1; i++){
         let sub = s[i]+s[i+1];
-        if(revString.includes(sub)) return true;
+        hash.add(sub);
+    }
+
+    for(let i=0; i<s.length-1; i++){
+        let sub = revString[i] + revString[i+1];
+        if(hash.has(sub)) return true;
     }
 
     return false;
